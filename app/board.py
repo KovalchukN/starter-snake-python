1b29f7bafa288a9cec8bdc247ddd80d7958ded61
KovalchukN <35732693+KovalchukN@users.noreply.github.com>
2019-11-20 16:10:57 +0300

    Add files via upload

---
+++ app/board.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+UNOCCUPIED = -2
+OCCUPIED   = 3
+FOOD       = -3
+HEAD       = 4
+TAIL =2 
+
+STORED_BOARDS = {}
+
+
+def place_coordinates(board, coords, value):
+    x_coord = coords['x']
+    y_coord = coords['y']
+    board[y_coord, x_coord] = value
+    return
+
+
+def update_board(state):
+    board = np.array([[UNOCCUPIED] * state['board']['height']] * state['board']['width'])
+
+    board_state = state['board']
+    food_coords = board_state['food']
+    snakes = board_state['snakes']
+    my_body = state['you']['body']
+
+    for coord in food_coords:
+        place_coordinates(board, coord, FOOD)
+
+    for snake in snakes:
+        snake_body = snake['body']
+        for coord in snake_body[1:]:
+            place_coordinates(board, coord, OCCUPIED)
+        head_coord = snake_body[0]
+        place_coordinates(board, head_coord, len(snake_body))
+
+    for coord in my_body[1:]:
+        place_coordinates(board, coord, OCCUPIED)
+    my_head_coord = my_body[0]
+    place_coordinates(board, my_head_coord, HEAD)
+
+    # print('Updated board state for turn ' + str(state['turn']) + ':\n\n' + str(board) + '\n\n')
+
+    return board
